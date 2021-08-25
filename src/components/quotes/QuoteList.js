747a06e509114fc637451e399caf7ce11fc1b54e
D.Marius <77802969+DMariuss@users.noreply.github.com>
2021-08-25 22:39:45 +0300

    Added functionality for adding and using query strings in url using useHistory and useLocation hooks from react-router-dom and using query strings for sorting and reevaluating the QuoteList component
    Am implementat cele 2 hook-uri pt a adauga query-strings in URL-uri. Sortarea listei cu citate in functie de valoarea param query -sort-

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,10 +1,42 @@
 import { Fragment } from "react";
-
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
+//import useHistory, useLocation pt a folosi parametrii query 🢣 pt a retine sortarea(pt cand trimit link-ul altcuiva)
+import { useHistory, useLocation } from "react-router-dom";
+// useHistory 🢣 setez parametrii query; useLocation 🢣 preiau informatiile despre pagina incarcata/curenta --aici, stringul query
+
+//functie pt sortarea listei
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      // ascending will be true/false
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
-  const content = props.quotes.map((quote) => (
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search); // 🢢 constructor pus la dispozitie de catre browser pt a prelua datele dintr-un string query 🢣 mult mai convenabil, puteam face asta manual
+  //                            ⇧ intoarce un Obiect de tip URLSearchParams cu anumite metode
+
+  const changeSortingHandler = () => {
+    //history.push pt a trimite pe aceasta adresa(aici pt a seta parametrii query) si disponibilitatea de a ma reintoarce la pagina precedenta
+    history.push(
+      `/quotes?sort=${isSortingAscending ? "descending" : "ascending"}`
+    ); // orice utilizare a metodelor de pe acest obiect al hook-ului a duce la reevaluarea componentei
+    // ⇧ logica este buna, cumva am o intarziere
+  };
+
+  const isSortingAscending = queryParams.get("sort") === "ascending"; // variabila ce va modifica comportamentul
+
+  const sorted = sortQuotes(props.quotes, isSortingAscending);
+
+  const content = sorted.map((quote) => (
     <QuoteItem
       key={quote.id}
       id={quote.id}
@@ -15,6 +47,12 @@ const QuoteList = (props) => {
 
   return (
     <Fragment>
+      {/* Adaug buton pt sortare 🢣 click pt sortarea specificata */}
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>{content}</ul>
     </Fragment>
   );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,4 @@
+// componenta ce se va afisa in cazul in care url-ul este diferit de oricare dintre rutele prestabilite
 const NotFound = () => {
   return (
     <div className="centered">
